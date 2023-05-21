91c3af640637bb11f37716aaba198964baa2f550
Kanwon <bgw2001@gmail.com>
2023-05-21 19:46:06 +0900

    Web Viewer 버전 업!

--- web_view_android/lib/main.dart
+++ web_view_android/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
 
 void main() {
+  //Flutter 프레임 워크가 앱을 실행할 준비가 될때 까지 기다린다.
+  //WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     home: HomeScreen(),
   ));
--- web_view_android/lib/screen/home_screen.dart
+++ web_view_android/lib/screen/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse("https://blog.codefactory.ai");
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = "https://blog.codefactory.ai";
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
+  //final homeUrl = "https://blog.codefactory.ai";
   HomeScreen({Key? key}) : super(key:key);
 
   @override
@@ -19,23 +23,26 @@ class HomeScreen extends StatelessWidget {
             // hhtps:// -> 지금은 이거 사용, 이게 조금더 보안이 좋다.
             // 안드로이드와 IOS 2개다 http를 막고 있다. Why ? 보안적으로 좋지 않기 때문이다.
             // 그러나, 원한다면 풀수 있는 방법이 존재
-            if(controller == null){
-              return;
-            }
-            controller!.loadUrl(homeUrl);
+            //if(controller == null){
+            //  return;
+            //}
+            controller.loadRequest(homeUrl);
           },
               icon: Icon(
                 Icons.home,
               ),)
         ],
       ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller){
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: WebViewWidget(
+        controller: controller,
       ),
+      //WebView(
+      //  onWebViewCreated: (WebViewController controller){
+      //    this.controller = controller;
+      //  },
+      //  initialUrl: homeUrl,
+      //  javascriptMode: JavascriptMode.unrestricted,
+      //),
     );
   }
 }
