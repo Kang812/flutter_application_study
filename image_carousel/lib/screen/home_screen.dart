929683e9fa1f5aaf9119002ebb61d17a5302392f
Kanwon <bgw2001@gmail.com>
2023-06-11 17:40:26 +0900

    컨트롤러 추가

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,19 +12,30 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Timer? timer;
-
+  PageController controller = PageController(
+    initialPage: 0,);
 
   @override
   void initState() {
     super.initState();
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      print("Timer!");
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4){
+        nextPage = 0;
+      }
+
+      controller.animateToPage(nextPage, duration: Duration(seconds: 4), curve: Curves.linear);
+      //print("Timer!");
     });
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    controller.dispose();
+
     if(timer != null){
       timer!.cancel();
     }
@@ -32,8 +44,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1,2,3,4,5].map((e) => Image.asset('asset/img/image_$e.jpeg', fit: BoxFit.cover,)).toList(),
       ),
     );
