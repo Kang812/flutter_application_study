0acea997ceae086199f146735ebaf81240bd8409
DESKTOP-CQQBQCL\COM <bgw2001@gmail.com>
2023-07-23 18:52:35 +0900

    랜던 숫자 생성 및 새로운 screen으로 이동

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/screen/settings_screen.dart';
 import '../constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +12,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> randomNumbers = [
+    123,
+    456,
+    789
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,55 +32,106 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("랜덤숫자 생성기", style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w700,
-                  ),),
-                  IconButton(
-                    onPressed: (){},
-                    icon: Icon(
-                      Icons.settings,
-                      color: RED_COLOR,
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      123,
-                      456,
-                      789,
-                    ].map((x) => Row(
-                      children: x.toString()
-                          .split('')
-                          .map((y) => Image.asset('asset/img/$y.png', height: 70.0, width: 50.0,))
-                          .toList(),),
-                    ).toList()
-                  ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: RED_COLOR,
-                          ),
-                          onPressed: (){},
-                          child: Text("생성하기!"),
-                    ),
-                  ),
-                ],
-              ),
+              _Header(),
+              _Body(randomNumbers: randomNumbers,),
+              _Footer(onPressed: onRandomNumberGenerate)
             ],
           ),
         ),
       ),
+    );
+  }
+
+  onRandomNumberGenerate(){
+    final rand = Random();
+    final Set<int> newNumbers = {};
+
+    while(newNumbers.length != 3){
+      final number = rand.nextInt(1000);
+      newNumbers.add(number);
+    }
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("랜덤숫자 생성기", style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+          fontWeight: FontWeight.w700,
+        ),),
+        IconButton(
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (BuildContext context){
+                return  SettingsScreen();},
+              ),
+            );
+          },
+          icon: Icon(
+            Icons.settings,
+            color: RED_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+  const _Body({required this.randomNumbers, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: randomNumbers
+              .asMap()
+              .entries
+              .map((x) => Padding(
+            padding: EdgeInsets.only(bottom:x.key == 2?0:16.0),
+            child: Row(
+              children: x.value
+                  .toString()
+                  .split('')
+                  .map((y) => Image.asset('asset/img/$y.png', height: 70.0, width: 50.0,))
+                  .toList(),),
+          ),
+          ).toList()
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Footer({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: RED_COLOR,
+            ),
+            onPressed: onPressed,
+            child: Text("생성하기!"),
+          ),
+        ),
+      ],
     );
   }
 }
