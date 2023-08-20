3ad4d11b4855ca9c19f35e6414315a8f7b1a4ba8
DESKTOP-CQQBQCL\COM <bgw2001@gmail.com>
2023-08-20 21:12:33 +0900

    no message

--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
@@ -26,6 +27,31 @@ class RouteTwoScreen extends StatelessWidget {
                 Navigator.of(context).pushNamed('/three', arguments: 999);
               },
               child: Text("Push Named")
+          ),
+          ElevatedButton(
+            //push [HomeScreen, Route One, Route Two, Route Three]
+            //pop [HomeScreen, Route One, Route Two]
+            // push replace ment -> [HomeScreen, Route One, Route Two]순서로 바뀜 쯕, Route Two를 Route One으로 대치
+              onPressed: (){
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => RouteThreeeScreen(),));
+              },
+              child: Text('Push Replace Ment'),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).pushReplacementNamed('/three');
+            },
+            child: Text('Push Replace Ment Named'),
+          ),
+          ElevatedButton(
+            // 뒤의 Route를 전부 지움
+              onPressed: (){
+                Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (_) => RouteThreeeScreen(),), (route) => route.settings.name == '/',
+                );
+              },
+              child:Text('Push And Remove Until'),
           ),
         ]
     );
